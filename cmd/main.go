01d6e01b1c8507c5b073337f85712379f6d9f7f3
PfMartin <martinpfatrisch@gmail.com>
2025-08-31 17:37:45 +0200

    Created first documents and retrieves documents from database

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"PfMartin/upprove-mcp-server/config"
 	"PfMartin/upprove-mcp-server/internal/db"
+	"PfMartin/upprove-mcp-server/internal/models"
 	"PfMartin/upprove-mcp-server/logging"
+	"context"
 	"fmt"
 
 	"github.com/rs/zerolog/log"
@@ -19,9 +21,35 @@ func main() {
 		return
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+
 	upproveStore := db.NewMongoDbStore(conf.DBName, conf.DBUser, conf.DBPassword, conf.DBURI)
 
-	fmt.Println(upproveStore)
+	performanceRecordCreate := models.PerformanceRecordCreate{
+		Category:    "Sports",
+		Description: "Any Description",
+		Value:       "1",
+		Unit:        "hours",
+	}
 
+	insertedID, err := upproveStore.CreatePerformanceRecord(ctx, performanceRecordCreate)
+	if err != nil {
+		log.Err(err).Msg("failed to insert performance record`")
+		cancel()
+		return
+	}
+
+	fmt.Println(insertedID)
+
+	performanceRecords, err := upproveStore.GetAllPerformanceRecords(ctx)
+	if err != nil {
+		log.Err(err).Msg("failed to get performance records`")
+		cancel()
+		return
+	}
+
+	fmt.Println(performanceRecords)
+
+	defer cancel()
 	log.Info().Msg("Starting Upprove MCP Server...")
 }
---
+++ internal/db/performance_records.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"PfMartin/upprove-mcp-server/internal/models"
+	"context"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func (store *MongoDbStore) GetAllPerformanceRecords(ctx context.Context) ([]models.PerformanceRecord, error) {
+	var performanceRecords []models.PerformanceRecord
+
+	cursor, err := store.performanceRecordsCollection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Err(err).Msg("failed to aggregate author documents")
+		return performanceRecords, err
+	}
+	defer cursor.Close(ctx)
+
+	if err = cursor.All(ctx, &performanceRecords); err != nil {
+		log.Err(err).Msg("failed to parse performance records documents")
+		return performanceRecords, err
+	}
+
+	return performanceRecords, nil
+}
+
+func (store *MongoDbStore) CreatePerformanceRecord(ctx context.Context, performanceRecord models.PerformanceRecordCreate) (string, error) {
+	indexModel := mongo.IndexModel{
+		Keys:    bson.M{"description": 1},
+		Options: options.Index().SetUnique(true),
+	}
+
+	_, err := store.performanceRecordsCollection.Indexes().CreateOne(ctx, indexModel)
+	if err != nil {
+		log.Err(err).Msgf("performance record with description %s already exists", performanceRecord.Description)
+		return "", err
+	}
+
+	insertData := bson.M{
+		"category":    performanceRecord.Category,
+		"description": performanceRecord.Description,
+		"value":       performanceRecord.Value,
+		"unit":        performanceRecord.Unit,
+		"createdAt":   time.Now().Unix(),
+		"modifiedAt":  time.Now().Unix(),
+	}
+
+	insertResult, err := store.performanceRecordsCollection.InsertOne(ctx, insertData)
+	if err != nil {
+		log.Err(err).Msgf("failed to insert performance record with description %s", performanceRecord.Description)
+		return "", err
+	}
+
+	insertedID := insertResult.InsertedID.(primitive.ObjectID)
+
+	return insertedID.Hex(), nil
+}
